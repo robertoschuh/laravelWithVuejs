e6c7a0c1bc74add9bd6d2a244526b657b5cf00c6
Rob <robbyschuh@gmail.com>
2019-04-22 17:19:59 +0700

    Cannot read property 'keep' of undefined

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,24 +2,26 @@
 
 @section('content')
 
-<div class="row"   id="crud">>
-	<div class="col-xs-12">
-		<h1 class="page-header">CRUD Laravel y VUEjs</h1>
-	</div>
-	<div class="col-sm-7">
-		<a href="#" class="btn btn-primary pull-right" data-toggle="modal" data-target="#create">
-		Nueva tarea
-        </a>
-        
-		<tasks></tasks>
+	<div class="row"   id="crud">>
+		<div class="col-xs-12">
+			<h1 class="page-header">CRUD Laravel y VUEjs</h1>
+		</div>
+		<div class="col-sm-7">
+			<a href="#" class="btn btn-primary pull-right" data-toggle="modal" data-target="#create">
+			Nueva tarea
+			</a>
+			
+			<tasks></tasks>
+		</div>
+		<div class="col-sm-5">
+			<pre>
+				@{{ $data }}
+			</pre>
+		</div>
+		@include('create')
+		@include('edit')
 
 	</div>
-	<div class="col-sm-5">
-		<pre>
-			@{{ $data }}
-		</pre>
-	</div>
-</div>
 
 @endsection
 
